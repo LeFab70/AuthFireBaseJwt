145efb2969753c27d99f7c2f83425f8389272e06
LeFab70 <kouonang2002@gmail.com>
2025-07-06 19:32:15 -0300

    Initial Commit

---
+++ AuthFireBaseJwt/AuthFireBaseJwtApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthFireBaseJwtApp.swift
+//  AuthFireBaseJwt
+//
+//  Created by Fabrice Kouonang on 2025-07-06.
+//
+
+import SwiftUI
+
+@main
+struct AuthFireBaseJwtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthFireBaseJwt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AuthFireBaseJwt
+//
+//  Created by Fabrice Kouonang on 2025-07-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
